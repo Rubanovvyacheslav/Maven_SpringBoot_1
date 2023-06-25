ccdc18423958a53ef4379a7a33c441b713755600
Rubanov <Rubanov.work@gmail.com>
2023-06-25 16:31:32 +0300

    Исправленный вариант

--- src/main/java/pro/sky/java/course2/weblibrary1/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/weblibrary1/CalculatorServiceImpl.java
@@ -25,8 +25,12 @@ public class CalculatorServiceImpl implements CalculateService {
     }
 
     public String divide(int num1, int num2) {
-        int result = num1 / num2;
-        return num1 + "/" + num2 + "=" + result;
+        if (num2 == 0) {
+            return "Здесь на ноль делить нельзя";
+        } else {
+            double result = (double) num1 / num2;
+            return num1 + "/" + num2 + "=" + result;
+        }
+        }
     }
 
-}
